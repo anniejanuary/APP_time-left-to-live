4c33e6116064c38136856dca2ccf1139a5f29230
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-21 14:28:52 +0200

    Create time left to live.py

---
+++ time left to live.py
@@ -0,0 +1,8 @@
+age = input("What is your current age?")
+
+years=90-int(age)
+months=years*12
+weeks=years*52
+days=years*365
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
